a7c3053eedd70a12cd6f22fb601bc1230e7e93a6
stephane71 <stephanemaire71@gmail.com>
2018-01-18 12:27:21 +0100

    Search Page | component as a React PureComponent Search Page | handleLoadPage function & concat current event list itself - fake data to create an infinite list

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,19 +1,60 @@
+import Head from 'next/head';
+
 import Layout from '../components/Layout';
 import EventList from '../components/EventList';
 
-const Search = props => (
-  <Layout>
-    <EventList
-      data={props.eventList.strides.reduce(
-        (current, next) => (current = current.concat(next)),
-        []
-      )}
-    />
-  </Layout>
-);
+const getEventStructuredData = function(event) {
+  const jsonLD = {};
+  return JSON.stringify(jsonLD);
+};
+
+export default class Search extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.handleLoadPage = this.handleLoadPage.bind(this);
+  }
+
+  state = {
+    loading: false,
+    events: this.props.eventList.strides.reduce(
+      (current, next) => (current = current.concat(next)),
+      []
+    )
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Head>
+          <title>{`La Foulée | rechercher un evénement`}</title>
+          <script type={'application/ld+json'}>
+            {getEventStructuredData()}
+          </script>
+        </Head>
+
+        <EventList
+          data={this.state.events}
+          onLoadMore={this.handleLoadPage}
+          loading={this.state.loading}
+        />
+      </Layout>
+    );
+  }
+
+  handleLoadPage() {
+    console.log('handleLoadPage');
+
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState(({ events }) => ({
+        loading: false,
+        events: events.concat(events)
+      }));
+    }, 3000);
+  }
+}
 
 Search.getInitialProps = async function({ query }) {
   return { eventList: query.eventList };
 };
-
-export default Search;
