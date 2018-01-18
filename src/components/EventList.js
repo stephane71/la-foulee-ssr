6e98025a883c73cd5780b5d25ffca3b6e2db863d
stephane71 <stephanemaire71@gmail.com>
2018-01-18 12:30:08 +0100

    EventList | remove CellMeasurer and create getRowHeight - rq: we know heights ahead of rendering EventList | add loader at the end of the list EventList | trigger onLoadMore when reach a selected index in list - see PADDING_INDEX_LOAD_MORE

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -11,25 +11,22 @@ import EventListDate from './EventListDate';
 // There is no SSR with react-virtualized as its need client dimension
 import Loader from './Loader';
 
-const cache = new CellMeasurerCache({
-  defaultHeight: 73,
-  fixedWidth: true
-});
-
 const EVENT_LIST_ITEM_HEIGHT = 73;
 const EVENT_LIST_DATE_HEADER_HEIGHT = 49;
+const PADDING_INDEX_LOAD_MORE = 2;
 
 export default class EventList extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      stickyDate: props.data[0].date,
-      rendered: false
+      rendered: false,
+      stickyDate: props.data[0].date
     };
 
     this.rowRenderer = this.rowRenderer.bind(this);
     this.onRowsRendered = this.onRowsRendered.bind(this);
+    this.getRowHeight = this.getRowHeight.bind(this);
   }
 
   render() {
@@ -46,9 +43,8 @@ export default class EventList extends React.PureComponent {
             <List
               width={width}
               height={height - EVENT_LIST_DATE_HEADER_HEIGHT}
-              rowCount={data.length}
-              deferredMeasurementCache={cache}
-              rowHeight={cache.rowHeight}
+              rowCount={data.length + 1}
+              rowHeight={this.getRowHeight}
               onRowsRendered={this.onRowsRendered}
               rowRenderer={this.rowRenderer}
             />
@@ -58,25 +54,31 @@ export default class EventList extends React.PureComponent {
     );
   }
 
+  getRowHeight({ index }) {
+    if (index === this.props.data.length) return 73;
+    return index &&
+      this.props.data[index].date !== this.props.data[index - 1].date
+      ? 122
+      : 73;
+  }
+
   rowRenderer({ key, index, style, parent }) {
     const { data } = this.props;
-    return (
-      <CellMeasurer
-        cache={cache}
-        columnIndex={0}
-        key={key}
-        parent={parent}
-        rowIndex={index}
-      >
+    if (index === data.length)
+      return (
         <div style={style} key={key}>
-          {(index &&
-            data[index].date != data[index - 1].date && (
-              <EventListDate date={data[index].date} />
-            )) ||
-            ''}
-          <EventListItem data={data[index]} />
+          <Loader />
         </div>
-      </CellMeasurer>
+      );
+    return (
+      <div style={style} key={key}>
+        {(index &&
+          data[index].date != data[index - 1].date && (
+            <EventListDate date={data[index].date} />
+          )) ||
+          null}
+        <EventListItem data={data[index]} />
+      </div>
     );
   }
 
@@ -86,6 +88,9 @@ export default class EventList extends React.PureComponent {
     startIndex,
     stopIndex
   }) {
+    if (stopIndex + PADDING_INDEX_LOAD_MORE === this.props.data.length) {
+      if (!this.props.loading) this.props.onLoadMore();
+    }
     this.setState({
       rendered: true,
       stickyDate: this.props.data[startIndex].date
