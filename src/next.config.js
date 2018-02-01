4b048c3fb959cef975c528a4f38a10039dee90ec
stephane71 <stephanemaire71@gmail.com>
2018-02-01 13:05:11 +0100

    Webpack | add webpack-bundle-analyzer && npm command

--- src/next.config.js
+++ src/next.config.js
@@ -1,5 +1,8 @@
 const webpack = require('webpack');
 
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { ANALYZE } = process.env;
+
 console.log('- next.config.js -');
 console.log('NODE_ENV:', process.env.NODE_ENV);
 
@@ -7,9 +10,19 @@ module.exports = {
   // assetPrefix: isProd ? `https://assets.la-foulee.com` : '',
 
   webpack: (config, { buildId, dev }) => {
-    config.plugins.concat([
+    config.plugins.push(
       new webpack.ContextReplacementPlugin(/moment[\\/]locale$/, /^\.\/(fr)$/)
-    ]);
+    );
+
+    if (ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: 8888,
+          openAnalyzer: true
+        })
+      );
+    }
 
     return config;
   }
