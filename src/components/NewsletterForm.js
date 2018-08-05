bf22d5592a325c24320bb761f08de059efc7f849
stephane71 <stephanemaire71@gmail.com>
2018-08-05 14:09:38 +0200

    NewsletterForm | update input to type text & required NewsletterForm | handle server error

--- src/components/NewsletterForm.js
+++ src/components/NewsletterForm.js
@@ -2,7 +2,17 @@ import React from 'react';
 import css from 'styled-jsx/css';
 
 import { getFontSize, getSpacing } from '../styles-variables';
-import { getColor, white } from '../colors';
+import { getColor, white, dominant } from '../colors';
+
+function getErrorMessage(error) {
+  if (error === NO_EMAIL) return 'Un email doit être fourni';
+  if (error === INVALID_FORMAT)
+    return `Le format de cet e-mail n'est pas valide`;
+}
+
+// Server Errors
+const NO_EMAIL = 'EmailNotFound';
+const INVALID_FORMAT = 'InvalidFormat';
 
 const style = css`
   .NewsletterForm-Form {
@@ -33,40 +43,54 @@ const style = css`
     cursor: pointer;
     margin: 0;
   }
+
+  .NewsletterForm-ErrorMessage {
+    color: ${dominant};
+  }
 `;
 
 export class NewsletterForm extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = { value: '', sent: false };
+    this.state = { value: '', sent: false, error: null };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
   }
 
   render() {
-    if (this.state.sent) return <div>{'Merci pour votre inscription !'}</div>;
+    if (this.state.sent && !this.state.error)
+      return <div>{'Merci pour votre inscription !'}</div>;
+
     return (
-      <form onSubmit={this.handleSubmit} className={'NewsletterForm-Form'}>
-        <div>
-          <input
-            type={'text'}
-            value={this.state.value}
-            onChange={this.handleChange}
-            placeholder={'E-MAIL'}
-            className={'NewsletterForm-InputText'}
-          />
-        </div>
-        <div>
-          <input
-            type={'submit'}
-            value={'Envoyer'}
-            className={'NewsletterForm-InputSubmit'}
-          />
+      <div className={'NewsletterForm'}>
+        <form onSubmit={this.handleSubmit} className={'NewsletterForm-Form'}>
+          <div>
+            <input
+              type={'email'}
+              required={'required'}
+              value={this.state.value}
+              onChange={this.handleChange}
+              onFocus={this.handleFocus}
+              placeholder={'E-MAIL'}
+              className={'NewsletterForm-InputText'}
+            />
+          </div>
+          <div>
+            <input
+              type={'submit'}
+              value={'Envoyer'}
+              className={'NewsletterForm-InputSubmit'}
+            />
+          </div>
+        </form>
+        <div className={'NewsletterForm-ErrorMessage'}>
+          {this.state.error ? getErrorMessage(this.state.error) : null}
         </div>
         <style jsx>{style}</style>
-      </form>
+      </div>
     );
   }
 
@@ -74,16 +98,23 @@ export class NewsletterForm extends React.PureComponent {
     this.setState({ value: event.target.value });
   }
 
+  handleFocus(event) {
+    if (this.state.sent) this.setState({ sent: false, error: null });
+  }
+
   async handleSubmit(event) {
     event.preventDefault();
+    let error = null;
 
     try {
       await this.props.postNewsletterEmail(this.state.value);
     } catch (e) {
-      console.error(e);
+      if (e.response.status === 400) {
+        error = e.response.data.code;
+      }
     }
 
-    this.setState({ sent: true });
+    this.setState({ sent: true, error });
   }
 }
 
