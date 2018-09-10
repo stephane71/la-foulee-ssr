ccac18fe7013ccb8163e5dd8c3b1702c4c0ae9ea
stephane71 <stephanemaire71@gmail.com>
2018-09-10 12:01:43 +0200

    EventListNotFoundError | button with theme dominant && size s EventListNotFoundError | adapt margin

--- src/components/EventListNotFoundError.js
+++ src/components/EventListNotFoundError.js
@@ -5,18 +5,27 @@ import { getColor } from '../colors';
 
 const EventListNotFoundError = ({ onTriggerSearch }) => (
   <div className={'EventListNotFoundError'}>
-    <h3>{'Aucun résultat'}</h3>
+    <h2>{'Aucun résultat'}</h2>
     <p
     >{`Afin d'obtenir davantage de résultats, essayez notre moteur de cherche.\nVous pouvez ainsi trouver la commune de votre choix et la liste des évènements autour de cette commune.`}</p>
-    <Button onClick={onTriggerSearch}>{`Rechercher une commune`}</Button>
+    <Button
+      onClick={onTriggerSearch}
+      size={'s'}
+      theme={'dominant'}
+    >{`Rechercher une commune`}</Button>
     <hr />
     <style jsx>{`
       .EventListNotFoundError {
         padding: ${getSpacing('m')}px ${getSpacing('s')}px;
       }
 
+      .EventListNotFoundError > h2 {
+        margin-bottom: 0;
+      }
+
       .EventListNotFoundError > p {
         white-space: pre-line;
+        margin-top: 0;
       }
 
       .EventListNotFoundError > hr {
