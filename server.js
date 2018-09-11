e47772d7c01fd819e45f78d651df66b54946b897
stephane71 <stephanemaire71@gmail.com>
2018-09-11 11:48:38 +0200

    server | redirect url with trailing slash

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ const app = next({ dev, dir });
 const handle = app.getRequestHandler();
 const server = express();
 
+server.use(function(req, res, next) {
+  if (req.path.substr(-1) == '/' && req.path.length > 1) {
+    const query = req.url.slice(req.path.length);
+    res.redirect(301, req.path.slice(0, -1) + query);
+  } else {
+    next();
+  }
+});
+
 server.get('/sw.js', (req, res) => {
   var options = {
     root: __dirname + '/src/static/',
