db451ec5a3da6ba1821df95b1b55b4935e63a00e
stephane71 <stephanemaire71@gmail.com>
2018-09-12 11:24:42 +0200

    internals | update sitemaps creation - create sitemap for event list from the most populated 200 cities package | add sitemap

--- internals/sitemapBuilder.js
+++ internals/sitemapBuilder.js
@@ -1,7 +1,13 @@
 require('dotenv').config({ path: './internals/.env' });
 const AWS = require('aws-sdk');
 const fs = require('fs');
+const slug = require('slug');
+const sm = require('sitemap');
+const moment = require('moment');
 
+const communes = require('./communes');
+
+const BASE_URL = 'https://www.la-foulee.com';
 const AWSConfig = {
   secretAccessKey: process.env.secretAccessKey,
   accessKeyId: process.env.accessKeyId,
@@ -12,45 +18,109 @@ const AWSConfig = {
 AWS.config.update(AWSConfig);
 let dbDocClient = new AWS.DynamoDB.DocumentClient();
 
-const SITEMAP = 'src/static/sitemap.txt';
-const BASE_URL = 'https://www.la-foulee.com';
-const APP_URLS = [BASE_URL];
+const pause = () =>
+  new Promise((resolve, reject) => setTimeout(() => resolve(), 60000));
+
+const createSitemapGlobal = () => {
+  const urls = [
+    {
+      url: `/`,
+      changefreq: 'monthly',
+      lastmodISO: moment().format('YYYY-MM-DD')
+    }
+  ];
+
+  const sitemap = sm.createSitemap({
+    hostname: BASE_URL,
+    cacheTime: 600000, // 600 sec cache period
+    urls
+  });
+
+  fs.writeFileSync('./src/static/sitemap.xml', sitemap.toString());
+};
+
+let sitemapEventsRef = 0;
+const createSitemapEvents = async (lastEvaluatedKey = null) => {
+  const data = await getEvents(lastEvaluatedKey);
+  const { Items: events, LastEvaluatedKey } = data;
+
+  const urls = events.map(({ keyword, date }) => ({
+    url: `/event/${keyword}/${moment
+      .unix(date)
+      .utc()
+      .year()}`,
+    changefreq: moment.unix(date).isBefore(moment()) ? 'yearly' : 'monthly'
+    // lastmodISO: moment().format('YYYY-MM-DD')
+  }));
+
+  const sitemap = sm.createSitemap({
+    hostname: BASE_URL,
+    cacheTime: 600000, // 600 sec cache period
+    urls
+  });
+
+  fs.writeFileSync(
+    `./src/static/sitemap_events_${sitemapEventsRef}.xml`,
+    sitemap.toString()
+  );
+
+  if (LastEvaluatedKey) {
+    console.log('1 minute pause', LastEvaluatedKey);
+    await pause();
+    debugger;
+    sitemapEventsRef++;
+    await createSitemapEvents(LastEvaluatedKey);
+  }
+};
+
+const createSitmapLists = () => {
+  const urls = communes.map(c => ({
+    url: `/events/${slug(c, { lower: true })}`,
+    changefreq: 'weekly',
+    lastmodISO: moment().format('YYYY-MM-DD')
+  }));
+
+  const sitemap = sm.createSitemap({
+    hostname: BASE_URL,
+    cacheTime: 600000, // 600 sec cache period
+    urls
+  });
+
+  fs.writeFileSync('./src/static/sitemap_lists.xml', sitemap.toString());
+};
 
 build();
 
 async function build() {
-  const events = await getEvents();
+  console.log('See the code to create sitemaps');
+  // console.log('Building sitemap_lists');
+  // createSitmapLists();
+  // console.log('-- sitemap_lists updated');
 
-  fs.writeFileSync(SITEMAP, '');
+  console.log('Building sitemap_events');
+  await createSitemapEvents();
+  console.log('-- sitemap_events updated');
+  debugger;
 
-  const eventsURLs = events.map(
-    ({ keyword }) => `${APP_URLS}/event/${keyword}`
-  );
-
-  APP_URLS.concat(eventsURLs).forEach(url => {
-    fs.writeFileSync(SITEMAP, `${url}\n`, { flag: 'a' });
-  });
+  // createSitemapGlobal();
 }
 
-function getEvents() {
-  return JSON.parse(
-    fs.readFileSync('../competition-scraper/out/events_bis.json')
-  );
-
+function getEvents(lastEvaluatedKey = null) {
+  let params = {
+    TableName: process.env.EVENTS_TABLE
+  };
+  if (lastEvaluatedKey) {
+    params = { ...params, ExclusiveStartKey: lastEvaluatedKey };
+  }
   return new Promise((resolve, reject) => {
-    dbDocClient.scan(
-      {
-        TableName: process.env.EVENTS_TABLE
-      },
-      (err, data) => {
-        if (err) {
-          console.log(err, err.stack);
-          reject(err);
-          return;
-        }
-        debugger;
-        resolve(data.Items);
+    dbDocClient.scan(params, (err, data) => {
+      if (err) {
+        console.log(err, err.stack);
+        reject(err);
+        return;
       }
-    );
+      console.log('scan ok');
+      resolve(data);
+    });
   });
 }
