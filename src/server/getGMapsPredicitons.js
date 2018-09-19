6f3497748df194ac69709efda0030bd12d5487d9
stephane71 <stephanemaire71@gmail.com>
2018-09-19 09:44:56 +0200

    getGMapsPredicitons | add language fr in req

--- src/server/getGMapsPredicitons.js
+++ src/server/getGMapsPredicitons.js
@@ -14,6 +14,7 @@ module.exports = function(city) {
       input: city,
       components: { country: 'fr' },
       types: '(cities)',
+      language: 'fr',
       sessiontoken: ''
     })
     .asPromise()
