e85fc02b0b5d09d7fa8d25074a7210eb68ed72ec
stephane71 <stephanemaire71@gmail.com>
2018-10-09 15:24:45 +0200

    api | update event contribution endpoint

--- src/api.js
+++ src/api.js
@@ -25,7 +25,7 @@ function postNewsletterEmailArgs(email) {
 function postEventContributionArgs({ contribution, user }, { keyword, date }) {
   const params = {};
   // Template syntax follows url-template https://www.npmjs.com/package/url-template
-  const pathTemplate = `/contribution/event`;
+  const pathTemplate = `/event`;
   const method = "POST";
   const additionalParams = {};
   const body = { contribution, user, keyword, date };
